4bcda6b845468f2906098ee29acd0a5010c8120c
divyashree <divyashreekbcashift1@gmail.com>
2025-06-13 16:25:41 +0530

    blog

---
+++ myapp/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Header from './components/Header';
+import BlogList from './components/BlogList';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <BlogList />
+      
+    </div>
+  );
+}
+
+export default App;
